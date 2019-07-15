2dca9c4712fd008329aabe3862ba8e396528e1a4
jaybenaim <benaimjacob@gmail.com>
2019-07-15 13:56:15 -0400

    added deposit and withdraw methods

--- exercise1.py
+++ exercise1.py
@@ -10,9 +10,22 @@ class BankAccount:
     def __str__(self): 
         return f' Balance: {self.balance} , Interest Rate: {self.interest_rate} '
 
-
+    def deposit(self, amount): 
+        self.balance += amount 
+        return self.balance 
+    
+    def withdraw(self, amount): 
+        self.balance -= amount 
+        return self.balance 
+    
 jays = BankAccount(433, 10) 
 print(jays)
 print(jays.balance) 
+
+jays.deposit(222)
+print(jays.balance)
+jays.withdraw(600) 
+print(jays.balance) 
+
 
 
